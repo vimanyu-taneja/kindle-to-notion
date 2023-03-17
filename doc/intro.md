9ae1bf04aaca3a28471e08e7c91031c4c16492c1
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-03-17 16:47:55 +0000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kindle-to-notion
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kindle_to_notion/core_test.clj
@@ -0,0 +1,7 @@
+(ns kindle-to-notion.core-test
+  (:require [clojure.test :refer :all]
+            [kindle-to-notion.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
