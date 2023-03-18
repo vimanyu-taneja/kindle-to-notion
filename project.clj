02fd32d781535df131c7064e9d62e81e8950da99
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-03-18 11:16:44 +0000

    Parse My Clippings.txt file
    Separate out individual clippings, extract the data from each clipping, and group them by book

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.taoensso/timbre "5.2.1"]]
   :main ^:skip-aot kindle-to-notion.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/kindle_to_notion/core.clj
+++ src/kindle_to_notion/core.clj
@@ -1,7 +1,36 @@
 (ns kindle-to-notion.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]]
+            [clojure.string :as str]
+            [taoensso.timbre :as log]))
+
+(defn parse-clippings []
+  (let [lines ; separate out individual clippings
+        (->  (io/resource "My Clippings.txt")
+             slurp
+             (str/replace #"\ufeff|\r\n" "")
+             (str/split #"=========="))
+        items ; extract data from each clipping
+        (->> lines
+             (map #(re-find #"^([^\(]+\S)\s*\(([^\)]+)\)-\s*Your Highlight at location\s+(\d+)-(\d+)\s*\|\s*Added on ([A-Za-z]+,\s+\d+\s+[A-Za-z]+\s+\d{4}\s+\d+:\d+:\d+)\s*(.*)$" %))
+             (remove nil?)
+             (map rest)
+             (map #(zipmap [:title :author :start-loc :end-loc :added-at :text] %))
+             (map #(let [start (Integer/parseInt (:start-loc %))
+                         end   (Integer/parseInt (:end-loc %))]
+                     (assoc % :start-loc start :end-loc end))))
+        groups ; group by book
+        (->> items
+             (group-by :title)
+             (map (fn [[title items]]
+                    {:title     title
+                     :clippings (sort-by :start-loc items)})))]
+    (log/info "Lines:"  lines)
+    (log/info "Items:"  (pr-str items))
+    (log/info "Groups:" (pr-str groups))
+    groups))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (pprint (parse-clippings)))
