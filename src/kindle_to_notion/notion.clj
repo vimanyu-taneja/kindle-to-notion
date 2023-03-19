1dc65adf9dfc2e8021830fcd5e00f1f0df2664f8
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-03-19 01:28:29 +0000

    Log full error when request fails

--- src/kindle_to_notion/notion.clj
+++ src/kindle_to_notion/notion.clj
@@ -1,49 +1,59 @@
 (ns kindle-to-notion.notion
   (:require [clj-http.client :as http]
             [clojure.data.json :as json]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [taoensso.timbre :as log]))
 
 (defn get-notion-db-entry [title]
-  (let [token    (env :notion-key)
-        db-id    (env :notion-db-id)
-        url      (format "https://api.notion.com/v1/databases/%s/query" db-id)
-        headers  {"Authorization"  (str "Bearer " token)
-                  "Notion-Version" "2022-06-28"
-                  :content-type    "application/json"}
-        body     (json/write-str
-                  {"filter" {"property"  "Title"
-                             "rich_text" {"equals" title}}})
-        response (http/post
-                  url
-                  {:headers headers
-                   :body    body})]
-    (json/read-str (:body response))))
+  (try
+    (let [token    (env :notion-key)
+          db-id    (env :notion-db-id)
+          url      (format "https://api.notion.com/v1/databases/%s/query" db-id)
+          headers  {"Authorization"  (str "Bearer " token)
+                    "Notion-Version" "2022-06-28"
+                    :content-type    "application/json"}
+          body     (json/write-str
+                    {"filter" {"property"  "Title"
+                               "rich_text" {"equals" title}}})
+          response (http/post
+                    url
+                    {:headers headers
+                     :body    body})]
+      (json/read-str (:body response)))
+    (catch Throwable e
+      (log/error "Unable to get Notion DB entry:" e))))
 
 (defn get-children-blocks [page-id]
-  (let [token    (env :notion-key)
-        url      (format "https://api.notion.com/v1/blocks/%s/children" page-id)
-        headers  {"Authorization"  (str "Bearer " token)
-                  "Notion-Version" "2022-06-28"}
-        response (http/get
-                  url
-                  {:headers headers})]
-    (json/read-str (:body response))))
+  (try
+    (let [token    (env :notion-key)
+          url      (format "https://api.notion.com/v1/blocks/%s/children" page-id)
+          headers  {"Authorization"  (str "Bearer " token)
+                    "Notion-Version" "2022-06-28"}
+          response (http/get
+                    url
+                    {:headers headers})]
+      (json/read-str (:body response)))
+    (catch Throwable e
+      (log/error "Unable to get children blocks:" e))))
 
 (defn update-block-text [block-id text]
-  (let [token    (env :notion-key)
-        url      (str "https://api.notion.com/v1/blocks/" block-id)
-        headers  {"Authorization"  (str "Bearer " token)
-                  "Notion-Version" "2022-06-28"
-                  :content-type    "application/json"}
-        body     (json/write-str
-                  {"type"      "paragraph"
-                   "paragraph" {"rich_text"
-                                [{"type" "text"
-                                  "text" {"content" text
-                                          "link"    nil}}]
-                                "color"  "default"}})
-        response (http/patch
-                  url
-                  {:headers headers
-                   :body    body})]
-    (json/read-str (:body response))))
+  (try
+    (let [token    (env :notion-key)
+          url      (str "https://api.notion.com/v1/blocks/" block-id)
+          headers  {"Authorization"  (str "Bearer " token)
+                    "Notion-Version" "2022-06-28"
+                    :content-type    "application/json"}
+          body     (json/write-str
+                    {"type"      "paragraph"
+                     "paragraph" {"rich_text"
+                                  [{"type" "text"
+                                    "text" {"content" text
+                                            "link"    nil}}]
+                                  "color"  "default"}})
+          response (http/patch
+                    url
+                    {:headers headers
+                     :body    body})]
+      (json/read-str (:body response)))
+    (catch Throwable e
+      (log/error "Unable to update block text:" e))))
